ed92fc136a24858d9eab32ed63bf5f68159f7beb
ESTAZOLAM <127852264+Raelserlight@users.noreply.github.com>
2026-07-01 23:50:19 +0700

    Update Grudge.js

--- backend/models/Grudge.js
+++ backend/models/Grudge.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const grudgeSchema = new mongoose.Schema({
   text: { type: String, required: true },
   isResolved: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
-  userId: { type: String, required: true }
+  createdAt: { type: Date, default: Date.now },
+  userId: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Grudge', grudgeSchema);
